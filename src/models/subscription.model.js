52792ff58154063fb4dff80921ab4270acb8e995
yubraj Mahanta <yubrajmahanta@gmail.com>
2026-02-07 23:04:03 +0530

    refactor subscription model: improve code formatting and structure for clarity

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,13 +1,18 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-const subscriptionSchema =new Schema({
-subscriber:{
-    type:Schema.Types.ObjectId,
-    ref:'User'
-},
-channel:{
-    type:Schema.Types.ObjectId,
-    ref:'User'
-}
-},{timestamps:true})
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
+    
